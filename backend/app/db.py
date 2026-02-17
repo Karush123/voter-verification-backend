d673ffcaa38b19251ee14aae7cd5a8469b4d2851
Karush123 <165473270+Karush123@users.noreply.github.com>
2026-02-17 21:45:56 +0530

    Update database connection string in db.py
    Replaced environment variable DATABASE_URL with hardcoded connection string.

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,4 +2,6 @@ import psycopg2
 import os
 
 def get_connection():
-    return psycopg2.connect(os.environ["DATABASE_URL"])
+    DATABASE_URL = "postgresql://voter_db_i5mt_user:1l3LLcTIr8kj6AE9IOpcuojnrOtK1iHf@dpg-d67etvk9c44c73dj8ucg-a.singapore-postgres.render.com/voter_db_i5mt"
+    return psycopg2.connect(DATABASE_URL)
+
