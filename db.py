9e98ecde6554659404cf7b718fee701d47433e20
Karush123 <165473270+Karush123@users.noreply.github.com>
2026-02-18 19:50:02 +0530

    Use DATABASE_URL for database connection
    Refactor database connection to use DATABASE_URL variable.

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import os
 import psycopg2
 
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
 def get_connection():
-    return psycopg2.connect(
-        os.environ.get("DATABASE_URL"),
-        sslmode="require"
-    )
+    return psycopg2.connect(DATABASE_URL)
