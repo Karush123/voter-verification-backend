37575b71d87963b271cbab7dd58357373b660f5e
Karush123 <165473270+Karush123@users.noreply.github.com>
2026-02-18 10:19:45 +0530

    Delete backend/app directory

--- backend/app/db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        "postgresql://voter_db_i5mt_user:1l3LLcTIr8kj6AE9IOpcuojnrOtK1iHf@dpg-d67etvk9c44c73dj8ucg-a.singapore-postgres.render.com/voter_db_i5mt",
-        sslmode="require"
-    )
-  
-
